62c382f3f5be5754c5feddb57d0d7f1a70b810c0
Wuway <contactdieter@gmail.com>
2025-08-19 20:18:58 +0200

    Oprettet og implementeret klasserne Order og OrderItem Ændringer i Order klassen i forhold til databasemodellen: - implementeret enum OrderStatus og property Status af type OrderStatus (i stedet for Status som int i dbmodel) - enum PayMethod og property PaymentMethod af type PayMethod (i stedet for PaymentMethod som int i dbmodel)

---
+++ Model/Order.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopTeam7.Model
+{
+    internal class Order
+    {
+        //OrderStatus enum til at repræsentere ordre status?
+        public enum OrderStatus
+        {
+            Pending,
+            Processing,
+            Completed,
+            Cancelled
+        }
+        //PaymentMethod enum til at repræsentere betalingsmetode?
+        public enum PayMethod
+        {
+            CreditCard,
+            PayPal,
+            BankTransfer,
+            MobilePay
+        }
+
+        public int OrderId { get; set; }
+        //nullable hvis vi tillader folk til at lave ordre uden at lave en konto?
+        public Customer UserId { get; set; } = null;
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public double OrderTotal { get; set; }
+        //paymentmethod int?
+        public PayMethod PaymentMethod { get; set; }
+
+
+
+
+        public Order(int orderId, Customer userId, DateTime date, double orderTotal, PayMethod paymentMethod, OrderStatus status)
+        {
+            OrderId = orderId;
+            Date = date;
+            Status = status;
+            OrderTotal = orderTotal;
+            PaymentMethod = paymentMethod;
+            UserId = userId;
+        }
+
+        public override string ToString()
+        {
+            return $"Order ID: {OrderId}, User: {UserId?.Name ?? "Guest"}, Date: {Date}, Status: {Status}, Total: {OrderTotal:C}, Payment Method: {PaymentMethod}";
+        }
+    }
+}
---
+++ Model/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopTeam7.Model
+{
+    internal class OrderItem
+    {
+        public required Order OrderId { get; set; }
+        public Product ProductId { get; set; }
+        public double Amount { get; set; }
+
+
+        public OrderItem(Order orderId, Product productId, double amount)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+            Amount = amount;
+        }
+    }
+}
