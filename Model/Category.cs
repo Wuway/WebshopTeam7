fd27b33a67ffb5467fa76ce2312a1038dc816fde
Benjamin Leonhardt <bjle71347@edu.ucl.dk>
2025-08-19 15:45:17 +0200

    Added Category and Point-transaction models

---
+++ Model/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopTeam7.Model;
+
+public class Category
+{
+    public int Id { get; private set; }
+    private string _name;
+    public string Name { get; set; }
+
+    public Category(string name, int Id = 0)
+    {
+        this.Name = name;
+    }
+}
---
+++ Model/Point_Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopTeam7.Model;
+
+public class Point_Transaction
+{
+    private Customer userId;
+    private int amount;
+    private Order order;
+
+    public Point_Transaction(Customer userId, int amount, Order order)
+    {
+        this.userId = userId;
+        this.amount = amount;
+        this.order = order;
+    }
+}
