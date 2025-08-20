192d733114a4aecc9c2ce7de4b7256fd7a777ca1
Cecilie <ceciliek1989@outlook.com>
2025-08-20 09:48:14 +0200

    CartItem

---
+++ Model/CartItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopTeam7.Model;
+
+internal class CartItem
+{
+    public int CartId { get; set; }   //CartId bruges til at identificere den specifikke kurv, som produktet er i
+    public int ProductId { get; set; } // ProductId bruges til at identificere det specifikke produkt i kurven
+    public double Amount { get; set; } // Amount er antallet af dette produkt i kurven
+
+    public CartItem(int productId, int cartId, int amount) // Konstruktøren initialiserer egenskaberne for CartItem-objektet
+    {
+        ProductId = productId;
+        CartId = cartId;
+        Amount = amount;
+    }
+
+}
